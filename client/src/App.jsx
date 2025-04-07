3fe09ab2c8f1730699090535b4f9714b97d48faa
Greycin Kim <kimgreycin@gmail.com>
2025-04-06 18:02:25 -0600

    Logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
-
+import logo from '/JBCH_Logo.png';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className={'text-center'}>Hello World</h1>
+      <>
+        <h1 className={'text-center text-color'}>Hello World</h1>
+      <div className={'container'}>
+        <img className={'logo'} src={logo} alt="logo" />
+      </div>
+      </>
+
   )
 }
 
